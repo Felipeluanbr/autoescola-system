d4e7c1ee536c8cb584fabd9aace871b90e71a290
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-03-16 07:33:46 -0300

    feat: Adicionar animação com JavaScript

--- app/pages/landing-page/landing.js
+++ app/pages/landing-page/landing.js
@@ -1,13 +1,11 @@
 // Aguardar que o DOM esteja totalmente carregado
 document.addEventListener("DOMContentLoaded", () => {
- 
   // pegar todos os botões
   const buttons = document.querySelectorAll(".category-btn");
-  
 
   // pegar todos os cards
   const categories = document.querySelectorAll(".category");
-  
+
   // Definir o primeiro botão como ativo por padrão
   if (buttons.length > 0) {
     buttons[0].classList.add("active");
@@ -21,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     button.addEventListener("click", (e) => {
       e.preventDefault();
-      
+
       // Remover classe active de todos os botões
       buttons.forEach((btn) => {
         btn.classList.remove("active");
@@ -29,28 +27,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Adicionar classe active ao botão clicado
       button.classList.add("active");
-     
 
       // remover active de todos os cards
       categories.forEach((category) => {
         category.classList.remove("category-active");
-       
       });
 
       // descobrir a categoria do botão
       const categoryName = button.classList[1];
-      
 
       // encontrar o card correspondente
       const target = document.getElementById(categoryName);
-     
+
       // ativar o card
       if (target) {
         target.classList.add("category-active");
-        
       } else {
-       
       }
     });
+  });
+});
+
+const buttonAcordion = document.querySelectorAll(".accordion");
+const cardPanel = document.querySelectorAll(".panel");
+
+buttonAcordion.forEach((button) => {
+  button.addEventListener("click", () => {
+    const panel = button.nextElementSibling;
+
+    panel.classList.toggle("activeAcordion");
+
+    button.classList.toggle("activeButtonAcordion")
   });
 });
