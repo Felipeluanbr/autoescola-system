8d85a5e90abd33697e97c13c544ec0c2443e7a9a
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-03-29 11:05:01 -0300

    feat: Adicionando a sidbar

--- aplication/frontend/system/aluno/aluno.js
+++ aplication/frontend/system/aluno/aluno.js
@@ -4,6 +4,7 @@ import { componentsSystem } from "../global/services/loadComponetsSystem.js";
   console.log("Iniciando carregamento do header...");
   try {
     await componentsSystem("header", "../global/components/header/header.html");
+    await componentsSystem("sidbar", "../global/components/sidbar/sidbarSystem.html");
     console.log("Header carregado com sucesso!");
   } catch (error) {
     console.error("Erro ao carregar header:", error);
